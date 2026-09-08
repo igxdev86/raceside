b588dd93b33bf9cb5d7d8962fbe35204f7c559af
igxdev86 <ryan@sm-i.co.uk>
2026-09-08 23:56:32 +0200

    nav: PREDICT

--- nav.js
+++ nav.js
@@ -20,7 +20,7 @@
       ['/ourodds.html', 'PRICE'],
     ]],
     ['ANGLES', [
-      ['/thewr.html', 'WR', ['TIPTREE', '/tiptree.html'], ['FIT', '/fit.html'], ['GAP', '/gap.html'], ['SIGNALS', '/signals.html'], ['OPTIMAL', '/optimal.html'], ['FRAME', '/tricast.html'], ['FIXTURES', '/fixtures.html']],
+      ['/thewr.html', 'WR', ['TIPTREE', '/tiptree.html'], ['FIT', '/fit.html'], ['GAP', '/gap.html'], ['SIGNALS', '/signals.html'], ['OPTIMAL', '/optimal.html'], ['FRAME', '/tricast.html'], ['FIXTURES', '/fixtures.html'], ['PREDICT', '/racingpredict.html']],
       ['/firstpairs.html', 'PAIRS'],
       ['/thehour.html', 'HOUR'],
       ['/martingale.html', 'MGALE'],
