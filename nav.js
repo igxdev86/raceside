027d5e50530c72c99bc705a9c939a761c53835fa
igxdev86 <ryan@sm-i.co.uk>
2026-09-08 12:46:01 +0200

    nav: FIXTURES

--- nav.js
+++ nav.js
@@ -20,7 +20,7 @@
       ['/ourodds.html', 'PRICE'],
     ]],
     ['ANGLES', [
-      ['/thewr.html', 'WR', ['TIPTREE', '/tiptree.html'], ['FIT', '/fit.html'], ['GAP', '/gap.html'], ['SIGNALS', '/signals.html'], ['OPTIMAL', '/optimal.html'], ['FRAME', '/tricast.html']],
+      ['/thewr.html', 'WR', ['TIPTREE', '/tiptree.html'], ['FIT', '/fit.html'], ['GAP', '/gap.html'], ['SIGNALS', '/signals.html'], ['OPTIMAL', '/optimal.html'], ['FRAME', '/tricast.html'], ['FIXTURES', '/fixtures.html']],
       ['/firstpairs.html', 'PAIRS'],
       ['/thehour.html', 'HOUR'],
       ['/martingale.html', 'MGALE'],
